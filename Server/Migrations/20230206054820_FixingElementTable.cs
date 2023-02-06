ae3fe9b8886b198a9cf4de34e8a91d9d91858879
Abser <rupok.diu@gmail.com>
2023-02-06 11:55:55 +0600

    Element CRUD

---
+++ Server/Migrations/20230206054820_FixingElementTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOMSBlazorApp.Server.Migrations
+{
+    public partial class FixingElementTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
